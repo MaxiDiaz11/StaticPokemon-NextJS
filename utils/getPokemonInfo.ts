bbaa89f910d9ef5c4320131285e977be38b6a0a8
Maxi Diaz <maxi.diaz66@gmail.com>
2023-02-10 12:27:40 -0300

    add  ISR and ISG

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,13 +2,17 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`);
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`);
 
-  const pokemon = {
-    name: data.name,
-    id: data.id,
-    sprites: data.sprites,
-  };
+    const pokemon = {
+      name: data.name,
+      id: data.id,
+      sprites: data.sprites,
+    };
 
-  return pokemon;
+    return pokemon;
+  } catch (error) {
+    return null;
+  }
 };
